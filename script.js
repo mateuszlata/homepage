2b72ba0159c7091b55e5abc35f2ba55cc001bbef
Mateusz Łata <mateuszlata@gmail.com>
2020-05-19 17:11:11 +0200

    initial commit Matthew Patch international NASA INDUSTRIES motherfuckers

---
+++ script.js
@@ -0,0 +1,7 @@
+const name = 'Maciek';
+const guestName = 'Stefan'
+const age = 29;
+
+// console.log('Siema, piss YOOO!');
+// console.log(`Siema mam na imię ${name} i mam ${age} lat`);
+console.log(`Witaj ${guestName}, nazwywam się ${name} i mam ${age} lat!`)
